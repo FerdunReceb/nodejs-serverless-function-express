d28d3032d7fe1846ae7fcb4f467dc32e50978b6b
FerdunReceb <galatasarayfy@gmail.com>
2025-09-05 07:23:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const PORT = process.env.PORT || 3000;
 app.get("/proxy", async (req, res) => {
   try {
     const response = await fetch("https://bit.ly/bysblogg", { redirect: "follow" });
+
+    // Sadece origin alıyoruz: https://www.baysansli614.com
+    const domain = new URL(response.url).origin;
+
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.json({ domain: response.url });
+    res.json({ domain });
   } catch (err) {
     res.status(500).json({ error: "Domain alınamadı", details: err.toString() });
   }
