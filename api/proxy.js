29f7ef20ce13ab1064ef8a4bdabf9759d7240ecf
FerdunReceb <galatasarayfy@gmail.com>
2025-09-05 07:06:04 +0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,8 +1,11 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import fetch from "node-fetch";
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("https://bit.ly/bysblogg", { redirect: "follow" });
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json({ domain: response.url });
+  } catch (err) {
+    res.status(500).json({ error: err.toString() });
+  }
 }
